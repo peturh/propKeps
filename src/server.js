27246bc8ffbd0d12c2f0501b32f2cf9a25f84ffe
lxove <love.jonsson@gmail.com>
2021-03-20 12:36:53 +0100

    first endpoint

--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
+const app = polka();
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const myMw = (req, res, next) => {
+  console.log("Loggar middleware");
+  req.mod = "Trams";
+  next();
+};
+
+app.get("/propkeps", myMw, (req, res) => {
+  console.log("propkepsget: Working");
+  res.end(`Success: ${req.mod}`);
+});
+
+app
+  .use(
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
